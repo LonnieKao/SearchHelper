19fd4b6ac0ff89487685ffa8b1c72bf590111fc8
Jia-Lung_Kao <Jia-Lung_Kao@syscom.com.tw>
2024-05-08 09:52:49 +0800

    改為非同步，避免視窗凍結

--- BingSearchHelper/frmMain.cs
+++ BingSearchHelper/frmMain.cs
@@ -13,7 +13,7 @@
     using System.Windows.Forms;
     using System.Xml.Linq;
     using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
-    
+
 
     public partial class frmMain : Form
     {
@@ -35,7 +35,7 @@
         private void Form1_Load(object sender, EventArgs e)
         {
             wvMain.Source = new Uri($@"https://www.bing.com/");
-            
+
             keyWards = File.ReadAllLines("keywords.txt");
         }
 
@@ -56,12 +56,15 @@
                 var Key = keyWards[lastIdx];
                 keyIdx.Remove(lastIdx);
 
-                wvMain.ExecuteScriptAsync($@"document.getElementById('sb_form_q').value = '{Key}';");
                 Task.Delay(delay).Wait();
-                wvMain.ExecuteScriptAsync(@"document.getElementById('sb_form_go').click();");
+                wvMain.Invoke((Action)(() =>
+                {
+                    wvMain.ExecuteScriptAsync($@"document.getElementById('sb_form_q').value = '{Key}';");
+                    wvMain.ExecuteScriptAsync(@"document.getElementById('sb_form_go').click();");
+                }));
 
                 int.TryParse(lblSearchCount.Text, out int count);
-                lblSearchCount.Text = (count + 1).ToString();
+                lblSearchCount.Invoke((Action)(() => { lblSearchCount.Text = (count + 1).ToString(); }));
             }
         }
 
@@ -72,7 +75,7 @@
                 MessageBox.Show("參數設定錯誤");
                 return;
             }
-            
+
             rand = new Random();
 
             for (int i = 0; i < SearchNum; i++)
@@ -94,8 +97,16 @@
 
         private void wvMain_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
         {
-            Task.Delay(1000).Wait();
-            ExeSerach();
+            NextSerach();
+        }
+
+        private void NextSerach()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                Task.Delay(1000).Wait();
+                ExeSerach();
+            });
         }
 
 
@@ -112,7 +123,7 @@
 
         private void btnFirst_Click(object sender, EventArgs e)
         {
-            wvMain.CoreWebView2.Settings.UserAgent = "Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Mobile Safari/537.36 Edge/18.22621";
+            wvMain.CoreWebView2.Settings.UserAgent = "Mozilla/5.0 (Linux; Android 9; ASUS_X00TDB Build/PKQ1; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/120.0.6099.144 Mobile Safari/537.36";
 
             wvMain.Source = new Uri($@"https://www.bing.com/search?q=test");
 
