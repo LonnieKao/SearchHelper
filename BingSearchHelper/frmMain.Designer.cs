7292ea3ff8258c228bb0039ede503337c65fc1c1
Jia-Lung_Kao <Jia-Lung_Kao@syscom.com.tw>
2024-06-28 09:04:57 +0800

    加入手機模式。

--- BingSearchHelper/frmMain.Designer.cs
+++ BingSearchHelper/frmMain.Designer.cs
@@ -34,8 +34,8 @@
             this.txtRangEnd = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.btnFirst = new System.Windows.Forms.Button();
+            this.lblMode = new System.Windows.Forms.Label();
+            this.btnChangeMode = new System.Windows.Forms.Button();
             this.lblSearchCount = new System.Windows.Forms.Label();
             this.txtSearchTime = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -52,18 +52,18 @@
             this.wvMain.CreationProperties = null;
             this.wvMain.DefaultBackgroundColor = System.Drawing.Color.White;
             this.wvMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.wvMain.Location = new System.Drawing.Point(3, 71);
+            this.wvMain.Location = new System.Drawing.Point(3, 53);
             this.wvMain.Name = "wvMain";
-            this.wvMain.Size = new System.Drawing.Size(971, 482);
+            this.wvMain.Size = new System.Drawing.Size(971, 500);
             this.wvMain.TabIndex = 0;
             this.wvMain.ZoomFactor = 1D;
             this.wvMain.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.wvMain_NavigationCompleted);
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(680, 18);
+            this.btnStart.Location = new System.Drawing.Point(845, 9);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.Size = new System.Drawing.Size(57, 25);
             this.btnStart.TabIndex = 1;
             this.btnStart.Text = "開始";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -71,7 +71,7 @@
             // 
             // txtRangStart
             // 
-            this.txtRangStart.Location = new System.Drawing.Point(102, 18);
+            this.txtRangStart.Location = new System.Drawing.Point(102, 11);
             this.txtRangStart.Name = "txtRangStart";
             this.txtRangStart.Size = new System.Drawing.Size(100, 22);
             this.txtRangStart.TabIndex = 2;
@@ -79,7 +79,7 @@
             // 
             // txtRangEnd
             // 
-            this.txtRangEnd.Location = new System.Drawing.Point(225, 18);
+            this.txtRangEnd.Location = new System.Drawing.Point(225, 11);
             this.txtRangEnd.Name = "txtRangEnd";
             this.txtRangEnd.Size = new System.Drawing.Size(100, 22);
             this.txtRangEnd.TabIndex = 3;
@@ -88,22 +88,22 @@
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.wvMain, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.23022F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87.76978F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(977, 556);
             this.tableLayoutPanel1.TabIndex = 4;
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.btnFirst);
+            this.panel1.Controls.Add(this.lblMode);
+            this.panel1.Controls.Add(this.btnChangeMode);
             this.panel1.Controls.Add(this.lblSearchCount);
             this.panel1.Controls.Add(this.txtSearchTime);
             this.panel1.Controls.Add(this.label3);
@@ -115,34 +115,32 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(971, 62);
+            this.panel1.Size = new System.Drawing.Size(971, 44);
             this.panel1.TabIndex = 1;
             // 
-            // label4
+            // lblMode
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(568, 41);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(33, 12);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "label4";
-            this.label4.Visible = false;
+            this.lblMode.AutoSize = true;
+            this.lblMode.Location = new System.Drawing.Point(547, 16);
+            this.lblMode.Name = "lblMode";
+            this.lblMode.Size = new System.Drawing.Size(53, 12);
+            this.lblMode.TabIndex = 12;
+            this.lblMode.Text = "目前模式";
             // 
-            // btnFirst
+            // btnChangeMode
             // 
-            this.btnFirst.Location = new System.Drawing.Point(860, 16);
-            this.btnFirst.Name = "btnFirst";
-            this.btnFirst.Size = new System.Drawing.Size(93, 23);
-            this.btnFirst.TabIndex = 9;
-            this.btnFirst.Text = "test";
-            this.btnFirst.UseVisualStyleBackColor = true;
-            this.btnFirst.Visible = false;
-            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            this.btnChangeMode.Location = new System.Drawing.Point(676, 11);
+            this.btnChangeMode.Name = "btnChangeMode";
+            this.btnChangeMode.Size = new System.Drawing.Size(123, 23);
+            this.btnChangeMode.TabIndex = 9;
+            this.btnChangeMode.Text = "切換模式並清除快取";
+            this.btnChangeMode.UseVisualStyleBackColor = true;
+            this.btnChangeMode.Click += new System.EventHandler(this.btnChangeMode_Click);
             // 
             // lblSearchCount
             // 
             this.lblSearchCount.AutoSize = true;
-            this.lblSearchCount.Location = new System.Drawing.Point(530, 23);
+            this.lblSearchCount.Location = new System.Drawing.Point(530, 16);
             this.lblSearchCount.Name = "lblSearchCount";
             this.lblSearchCount.Size = new System.Drawing.Size(11, 12);
             this.lblSearchCount.TabIndex = 8;
@@ -150,7 +148,7 @@
             // 
             // txtSearchTime
             // 
-            this.txtSearchTime.Location = new System.Drawing.Point(414, 18);
+            this.txtSearchTime.Location = new System.Drawing.Point(414, 11);
             this.txtSearchTime.Name = "txtSearchTime";
             this.txtSearchTime.Size = new System.Drawing.Size(100, 22);
             this.txtSearchTime.TabIndex = 7;
@@ -159,7 +157,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(352, 23);
+            this.label3.Location = new System.Drawing.Point(352, 16);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 12);
             this.label3.TabIndex = 6;
@@ -168,7 +166,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(208, 23);
+            this.label2.Location = new System.Drawing.Point(208, 16);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(11, 12);
             this.label2.TabIndex = 5;
@@ -177,7 +175,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 23);
+            this.label1.Location = new System.Drawing.Point(20, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 12);
             this.label1.TabIndex = 4;
@@ -191,7 +189,7 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "frmMain";
             this.Text = "BingSearchHelper";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.frmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.wvMain)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
@@ -213,8 +211,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblSearchCount;
-        private System.Windows.Forms.Button btnFirst;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnChangeMode;
+        private System.Windows.Forms.Label lblMode;
     }
 }
 
--- BingSearchHelper/frmMain.cs
+++ BingSearchHelper/frmMain.cs
@@ -13,6 +13,7 @@
     using System.Windows.Forms;
     using System.Xml.Linq;
     using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+    using System.Configuration;
 
 
     public partial class frmMain : Form
@@ -27,21 +28,42 @@
 
         Random rand = new Random();
 
+        EMode currentMode = EMode.pc;
+
+        string MobileAgent = ConfigurationManager.AppSettings["UserAgnet"];
+        enum EMode
+        {
+            mobile = 0,
+            pc = 1,
+        }
+
         public frmMain()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void frmMain_Load(object sender, EventArgs e)
         {
             wvMain.Source = new Uri($@"https://www.bing.com/");
+            Task.Delay(1000).Wait();
+            //if (!string.IsNullOrEmpty(Properties.Settings.Default.PhoneMode))
+            //{
+            //    lblMode.Text = "目前模式:mobile";
+            //    currentMode = EMode.mobile;
+            //   // wvMain.CoreWebView2.Settings.UserAgent = MobileAgent;
+            //}
+            //else
+            //{
+                lblMode.Text = "目前模式:PC";
+                currentMode = EMode.pc;
+            //}
 
             keyWards = File.ReadAllLines("keywords.txt");
         }
 
         private void Init(string KeyWord)
         {
-            wvMain.Source = new Uri($@"https://www.bing.com/search?q='{KeyWord}'");
+            wvMain.Source = new Uri($@"https://www.bing.com/search?q={KeyWord}");
 
             Task.Delay(2000).Wait();
         }
@@ -81,7 +103,7 @@
 
             rand = new Random();
 
-            for (int i = 0; i < SearchNum*2; i++)
+            for (int i = 0; i < SearchNum * 2; i++)
             {
                 int idx;
                 do
@@ -115,24 +137,49 @@
             });
         }
 
-
-        private void SetMobileMode()
+        private void btnChangeMode_Click(object sender, EventArgs e)
         {
-            wvMain.CoreWebView2.Settings.UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148";
+            if (MessageBox.Show("帳號將被登出!!", "警告", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.Cancel)
+            {
+                return;
+            }
 
+            ClearCache();
+
+            if (currentMode == EMode.mobile)
+            {
+                wvMain.CoreWebView2.Settings.UserAgent = null;
+                currentMode = EMode.pc;
+                lblMode.Text = "目前模式:PC";
+                //Properties.Settings.Default.PhoneMode = string.Empty;
+                //Properties.Settings.Default.Save();
+            }
+            else
+            {
+                wvMain.CoreWebView2.Settings.UserAgent = MobileAgent;
+                currentMode = EMode.mobile;
+                lblMode.Text = "目前模式:mobile";
+                //Properties.Settings.Default.PhoneMode = "true";
+                //Properties.Settings.Default.Save();
+            }
+            wvMain.Source = new Uri($@"https://www.bing.com/");
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void ClearCache()
         {
-            //wvMain.ExecuteScriptAsync(@"document.getElementById('sb_form_go').click();");
+            //wvMain.CoreWebView2.Profile.ClearBrowsingDataAsync();
+            //var dataKinds = CoreWebView2BrowsingDataKinds.AllProfile & ~CoreWebView2BrowsingDataKinds.Cookies;
+            //var dataKinds = CoreWebView2BrowsingDataKinds.FileSystems;
+            var dataKinds = CoreWebView2BrowsingDataKinds.AllProfile;
+            await wvMain.CoreWebView2.Profile.ClearBrowsingDataAsync(dataKinds);
         }
 
-        private void btnFirst_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
+            ClearCache();
+
             wvMain.CoreWebView2.Settings.UserAgent = "Mozilla/5.0 (Linux; Android 9; ASUS_X00TDB Build/PKQ1; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/120.0.6099.144 Mobile Safari/537.36";
-
             wvMain.Source = new Uri($@"https://www.bing.com/search?q=test");
-
         }
     }
 }
